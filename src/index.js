2354d6f5d1660a56f71bd81fe2ae80c86d1df06c
Eric F <ericdfanning@gmail.com>
2019-10-24 23:00:12 -0500

    added vanilla template for zeit now.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world!');
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const navLinks = document.querySelector('.nav-links');
+	const nav = document.querySelector('nav');
+
+console.log('burger is', burger)
+	burger.addEventListener('click', () => {
+		navLinks.classList.toggle('nav-links-active');
+		nav.classList.toggle('nav-active');
+	})
+}
+
+navSlide();
