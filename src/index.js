c956552bc382a0c2c320c6859f6c53ae3dbfa5ec
Eric F <ericdfanning@gmail.com>
2019-10-27 21:32:23 -0500

    responsive nav and mobile flyout menu

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
-	const navLinks = document.querySelector('.nav-links');
-	const nav = document.querySelector('nav');
+	const navLinks = document.querySelector('.mobile-nav__links');
+	// const nav = document.querySelector('nav');
 
-console.log('burger is', burger)
+console.log('burger is dude', navLinks)
 	burger.addEventListener('click', () => {
 		navLinks.classList.toggle('nav-links-active');
-		nav.classList.toggle('nav-active');
+		// nav.classList.toggle('nav-active');
+		burger.classList.toggle('toggle');
 	})
 }
 
