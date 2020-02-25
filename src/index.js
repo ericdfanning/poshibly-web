a4d0ffcc9f465b73653053e4323eced9f4f56055
Eric F <ericdfanning@gmail.com>
2020-02-24 22:46:29 -0600

    nav now working as fixed and still responsive with menu flyout. removed dist from indexed files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const navLinks = document.querySelector('.mobile-nav__links');
+	const bodyExcludingMenu = document.querySelector('.main-content')
 	// const nav = document.querySelector('nav');
 
 console.log('burger is dude', navLinks)
 	burger.addEventListener('click', () => {
 		navLinks.classList.toggle('nav-links-active');
-		// nav.classList.toggle('nav-active');
 		burger.classList.toggle('toggle');
-	})
+	});
+
+	bodyExcludingMenu.addEventListener('click', () => {
+		navLinks.classList.toggle('nav-links-active');
+		burger.classList.toggle('toggle');
+	});
 }
 
 navSlide();
