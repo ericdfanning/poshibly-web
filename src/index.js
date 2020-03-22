ad2667541cf0c49dbb522b8a2c6d196e9d6533bd
Eric F <ericdfanning@gmail.com>
2020-03-21 20:43:05 -0500

    added listners to close menu on certain area clicks

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const navLinks = document.querySelector('.mobile-nav__links');
-	const bodyExcludingMenu = document.querySelector('.main-content')
-	// const nav = document.querySelector('nav');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.mobile-nav__links');
+const bodyExcludingMenu = document.querySelector('.main-content');
+const menuItemsMobile = document.querySelectorAll('ul.mobile-nav__links li');
 
-console.log('burger is dude', navLinks)
+const navSlide = () => {
+
 	burger.addEventListener('click', () => {
 		navLinks.classList.toggle('nav-links-active');
 		burger.classList.toggle('toggle');
 	});
 
 	bodyExcludingMenu.addEventListener('click', () => {
-		navLinks.classList.toggle('nav-links-active');
-		burger.classList.toggle('toggle');
+		closeMenu();
 	});
+
+	for (let i = 0; i < menuItemsMobile.length; i++) {
+		menuItemsMobile[i].addEventListener('click', (e) => {
+			closeMenu();
+		});
+	}
+
+
+}
+
+const closeMenu = () => {
+	navLinks.classList.toggle('nav-links-active', false);
+	burger.classList.toggle('toggle', false);
 }
 
 navSlide();
