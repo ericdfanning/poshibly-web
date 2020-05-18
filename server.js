0bff69407863e93f425eb140cd4a19de0fc498b5
Eric F <ericdfanning@gmail.com>
2020-05-17 21:42:24 -0500

    a lot of layout changes. added rough overlay for privacy policy and terms of service

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var cors = require('cors');
+var fs = require('fs');
 
 var port = 8000
 app.set('port', port);
@@ -30,6 +31,43 @@ app.get('/main-menu.png', function(req, res) {
 	res.sendFile(path.join(__dirname, './main-menu.png'))
 })
 
-app.get('/activelySharing.png', function(req, res) {
-	res.sendFile(path.join(__dirname, './activelySharing.png'))
+app.get('/sharing.png', function(req, res) {
+	res.sendFile(path.join(__dirname, './sharing.png'))
+})
+
+app.get('/items', function(req, res) {
+
+	fs.readFile('../../../../Desktop/openHAB-state.txt', 'utf8', function(err, data) {
+	    if (err) throw err;
+	    let body = '';
+	    console.log('what is the data', JSON.parse(data).length)
+	    let results = JSON.parse(data);
+	    for (let i = 0; i < results.length; i++) {
+	    	if (results[i].state !== 'NULL') {
+	    		body += `<div><h2>${results[i].type}</h2><h4> Label: ${results[i].label}</h4><h5> Name: ${results[i].name}</h5>`
+	    		if (results[i].groupNames.length > 0) {
+	    			body += `<h6>group names: ${results[i].groupNames.reduce( (accum, item) => {
+	    				return accum + `<span>${item}, </span>`
+	    			}, '')}</h6>`
+	    		}
+	    		body += '</div>'
+	    	}
+	    }
+  		res.status(200)
+  		res.send(body)
+	    // console.log('whats the data', Object.values(JSON.parse(data)));
+	});
+})
+
+
+app.get('/sitemap', function(req, res) {
+
+	fs.readFile('../../../../Desktop/curl_sitemaps.txt', 'utf8', function(err, data) {
+	    if (err) throw err;
+	    console.log('sitemap', typeof data, data)
+		res.status(200)
+		res.send(data)
+
+	});
+
 })
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+const Mustache = require('mustache');
+const pricingOverlayTemplate = require('./templates/pricingOverlay.html');
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.mobile-nav__links');
 const bodyExcludingMenu = document.querySelector('.main-content');
 const menuItemsMobile = document.querySelectorAll('ul.mobile-nav__links li');
-const featureNavLink = document.querySelector('#Features');
+const featureNavLinks = document.querySelectorAll('#Features');
 const moreBelowCaret = document.querySelector('.more-below-caret__elm');
 const whatIsItSection = document.querySelector('.what-is-it');
 const featuresSection = document.querySelector('.features');
+const priceElement = document.querySelector('.price__value');
+const privacyPolicy = document.querySelector('.privacy-policy');
+const termsOfService = document.querySelector('.terms-of-service');
 
 
 const navSlide = () => {
@@ -16,7 +21,6 @@ const navSlide = () => {
 	});
 
 	bodyExcludingMenu.addEventListener('click', () => {
-		console.log('is the close anywhere firing???')
 		closeMenu();
 	});
 
@@ -33,15 +37,103 @@ const closeMenu = () => {
 }
 
 const initEventListeners = () => {
+	navSlide();
+	initNavLinkListeners();
+
 	moreBelowCaret.addEventListener('click', () => {
 		whatIsItSection.scrollIntoView();
 	})
 
-	featureNavLink.addEventListener('click', () => {
-		console.log('is it firing?')
-		featuresSection.scrollIntoView();
+	privacyPolicy.addEventListener('click', () => {
+		document.querySelector('.privacy-overlay').setAttribute('style', 'z-index: 5000; visibility: visible;');
+		document.querySelector('.main-body').setAttribute('style', 'opacity: 0.2;')
+		let pricingData = { test: 'HELLO DUDES!'}
+		let overlayContent = Mustache.render(pricingOverlayTemplate, pricingData)
+		document.querySelector('.privacy-overlay').innerHTML = overlayContent
+		const closeOverlay = document.querySelector('#close-overlay');
+		closeOverlay.addEventListener('click', () => {
+			document.querySelector('.privacy-overlay').setAttribute('style', 'z-index: -100; visibility: hidden;');
+			document.querySelector('.main-body').setAttribute('style', 'opacity: 1;')
+		})
+	})
+
+	termsOfService.addEventListener('click', () => {
+		const node = document.createElement("DIV");
+		node.setAttribute('class', 'terms-of-service-overlay');
+		const text = document.createTextNode("terms of service overlay");
+		node.appendChild(text)
+	  document.querySelector('body').append(node)
+	  
+	  const closeOverlay = document.querySelector('#close-overlay');
+	  closeOverlay.addEventListener('click', () => {
+	  	document.querySelector('.privacy-overlay').setAttribute('style', 'z-index: -100; visibility: hidden;');
+	  	document.querySelector('.main-body').setAttribute('style', 'opacity: 1;')
+	  })
 	})
 }
 
-navSlide();
+const initNavLinkListeners = () => {
+	for (let i = 0; i < featureNavLinks.length; i++) {
+		featureNavLinks[i].addEventListener('click', () => {
+			featuresSection.scrollIntoView();
+		})
+	}
+}
+
 initEventListeners();
+
+function isElementInViewport (el) {
+
+    // Special bonus for those using jQuery
+    // if (typeof jQuery === "function" && el instanceof jQuery) {
+    //     el = el[0];
+    // }
+
+    var rect = el.getBoundingClientRect();
+    // console.log('what is rect', rect, 'window', window.innerHeight, 'window width', window.innerWidth)
+
+    return (
+        rect.top >= 0 &&
+        // rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) /* or $(window).height() */
+        // rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
+function onVisibilityChange(el, callback) {
+    var old_visible = false;
+    return function () {
+        var visible = isElementInViewport(el);
+        if (visible != old_visible) {
+        	console.log('its visible', visible)
+            old_visible = visible;
+            if (typeof callback == 'function') {
+                callback();
+            }
+        }
+    }
+}
+
+let handler = onVisibilityChange(priceElement, () => {
+	priceElement.classList.add('animate-price')
+});
+
+ // Non-jQuery
+if (window.addEventListener) {
+    // addEventListener('DOMContentLoaded', handler, false);
+    // addEventListener('load', handler, false);
+    addEventListener('scroll', handler, false);
+    addEventListener('resize', handler, false);
+} else if (window.attachEvent)  {
+    // attachEvent('onDOMContentLoaded', handler); // Internet Explorer 9+ :(
+    // attachEvent('onload', handler);
+    attachEvent('onscroll', handler);
+    attachEvent('onresize', handler);
+}
+console.log('FAAAARRRRTTTT')
+if (window.location.hash.includes('privacypolicy') || window.location.hash.includes('termsofservice')) {
+	document.querySelector('.privacy-overlay').setAttribute('style', 'z-index: 5000; visibility: visible;');
+	// document.querySelector('.privacy-policy-overlay').setAttribute('z-index', '5000');
+	document.querySelector('.main-body').setAttribute('style', 'opacity: 0.2;')
+}
+
